0ecb6ef049763b8bc4835233bfd7a573057133f5
imeshcheryakova <imeshcher@chicagobooth.edu>
2015-07-26 00:21:21 -0500

    photo_complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,32 @@
 class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
+
   end
 
+  def show
 
+
+@photo_id = params["id"]
+
+
+db=Photo.find(@photo_id.to_i)
+
+@caption=db.caption
+@source=db.source
+
+end
+
+    def destroy
+      @photo_id = params["id"]
+      db=Photo.find(@photo_id.to_i)
+
+      db.destroy
+      @list_of_photos = Photo.all
+      render("index.html.erb")
+    end
+
+def new_form
+
+end
 end
